ff0cc41e3fc69917f610dca2d7454e47624aaf6d
Rylan Command <rylancommand95@gmail.com>
2019-10-21 22:27:57 -0230

    Add steps to check for data tidyness

--- scr/0.0 Load data and clean.R
+++ scr/0.0 Load data and clean.R
@@ -4,13 +4,64 @@
 
 ## 0.0 Load in data
 catch <- read.csv("data/cdis5016-all9sp.csv", header = TRUE)
+
+## Variables list
+# SpeciesCode: Three letter code corresponding to each species
+# YearC: Calendar year
+# Decade: Years collated into decades (1950, 1960... etc.)
+# FlagName: ICCAT Flag name
+# FleetCode: ICCAT Fleet code
+# Stock: Species related stock or management unit
+# GearGrp: Gear types used to capture tunas
+# SchoolType: Type of fishing operation
+# Trimester: Time strata (trimester 1, 2, 3, 4)
+# QuadID: ICCAT Quadrants
+# Lat5: Latitude of a 5x5 square
+# Lon5: Longitude of a 5x5 square
+# yLat5ctoid: Latitude (decimal degrees) centroid (Cartesian) of a 5x5 square
+# xLon5ctoid: Longitude (decimal degrees) centroid (Cartesian) of a 5x5 square
+# Catch_t: Nominal catches (tonnes)
+
+## 0.1 Check to see if the data are clean
+
+#### 0.1.1 Did the data load correctly? ####
 head(catch)
 str(catch)
+## Looks good
 
+#### 0.1.2 Are the data types correct? ####
+sapply(catch, class)
+## Looks good
+
+#### 0.1.3 Check for impossible values ####
+## Go through all of the numerical values to check if they are impossible
+sapply(catch[, sapply(catch, is.numeric)], range)  # Ranges look good!
+
+## Plot each numerical value to visually assess
+plot(catch$Year)
+plot(catch$Decade)
+plot(catch$Trimester)
+plot(catch$QuadID)
+plot(catch$Lat5)
+plot(catch$Lon5)
+plot(catch$yLat5ctoid)
+plot(catch$xLon5ctoid)
+plot(catch$Catch_t)
+## All look good!
+
+
+#### 0.1.4 Are factor levels correct? ####
+## Go through all factors and make sure levels are correct
+sapply(catch[,sapply(catch, is.factor)], levels)  # All factor levels look good!
+
+## I want to look specifically at swordfish, let's clean the dataset
 ## 0.1 Load in packages
 library(ggplot2)
 library(tidyr)
 library(dplyr)
+
+
+
 
 ## 0.2 Clean data to work with swordfish in north atlantic
 clean <- catch %>% 
